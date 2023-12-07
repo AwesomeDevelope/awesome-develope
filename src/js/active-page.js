1d61be46b2b959466349b33c92350a84de08b3c7
YanLozovskyi <yanlozovskiy@gmail.com>
2023-12-07 22:57:22 +0200

    added variables

--- src/js/active-page.js
+++ src/js/active-page.js
@@ -1,11 +1,23 @@
 const pagesEl = document.querySelectorAll('.header-nav-link');
 const windowPathName = window.location.pathname;
 
+// pagesEl.forEach(pageEl => {
+//   const pageLinkPathname = new URL(pageEl.href).pathname;
+
+//   if (
+//     (windowPathName === '/' && pageLinkPathname === '/') ||
+//     pageLinkPathname === windowPathName
+//   ) {
+//     pageEl.classList.add('active');
+//   }
+// });
+
 pagesEl.forEach(pageEl => {
   const pageLinkPathname = new URL(pageEl.href).pathname;
 
   if (
-    (windowPathName === '/' && pageLinkPathname === '/') ||
+    (windowPathName === '/awesome-develope/' &&
+      pageLinkPathname === '/awesome-develope/index.html') ||
     pageLinkPathname === windowPathName
   ) {
     pageEl.classList.add('active');
@@ -19,9 +31,21 @@ MobilePagesEl.forEach(pageEl => {
   const pageLinkPathname = new URL(pageEl.href).pathname;
 
   if (
-    (MobileWindowPathName === '/' && pageLinkPathname === '/') ||
+    (windowPathName === '/awesome-develope/' &&
+      pageLinkPathname === '/awesome-develope/index.html') ||
     pageLinkPathname === MobileWindowPathName
   ) {
     pageEl.classList.add('active');
   }
 });
+
+// MobilePagesEl.forEach(pageEl => {
+//   const pageLinkPathname = new URL(pageEl.href).pathname;
+
+//   if (
+//     (MobileWindowPathName === '/' && pageLinkPathname === '/') ||
+//     pageLinkPathname === MobileWindowPathName
+//   ) {
+//     pageEl.classList.add('active');
+//   }
+// });
